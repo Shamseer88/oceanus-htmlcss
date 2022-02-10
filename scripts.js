08671348c169d299d45e704b86ad78ec616a9754
Shamseer88 <alishamseer@gmail.com>
2022-02-10 17:09:57 +0530

    Var changes

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
 function myFunction() {
-    var x = document.getElementById("email").value;
-    document.getElementById("demo").innerHTML = x+ " is your email id";
+    var email = document.getElementById("email").value;
+    document.getElementById("display-email").innerHTML = email+ " is your email id";
   }
